d338fa415a0cf31b9f827c6f045a68390f7c2c69
Kuba <kuba.mistrz@o2.pl>
2018-12-27 13:36:26 +0100

    added towers with blocks and displayed them

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,43 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      firstTower: [
+        { id: 1, class: 'one' },
+        { id: 2, class: 'two' },
+        { id: 3, class: 'three' },
+        { id: 4, class: 'four' },
+      ],
+      secondTower: [],
+      thirdTower: [],
     };
   }
 
   render() {
+    const { firstTower, secondTower, thirdTower } = this.state;
     return (
-      <main />
+      <main>
+        <div className="tower">
+          {firstTower.map(bl => (
+            <div key={bl.id} className={bl.class}>
+              {bl.id}
+            </div>
+          ))}
+        </div>
+        <div className="tower">
+          {secondTower.map(bl => (
+            <div key={bl.id} className={bl.class}>
+              {bl.id}
+            </div>
+          ))}
+        </div>
+        <div className="tower">
+          {thirdTower.map(bl => (
+            <div key={bl.id} className={bl.class}>
+              {bl.id}
+            </div>
+          ))}
+        </div>
+      </main>
     );
   }
 }
