2809853a0c1a4bdd042405e936896d1a8129c5ed
Kuba <kuba.mistrz@o2.pl>
2019-01-11 18:17:06 +0100

    initialized App with redux provider and store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { store } from './store';
+import { Provider } from 'react-redux';
+import { AppContainer } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('root')
+);
