9314303247228e2a20d62d4d889ddd9d93ca5884
Kuba <kuba.mistrz@o2.pl>
2019-01-15 00:16:07 +0100

    added case which returns reset movesCounter

--- src/reducers/movesNumber.js
+++ src/reducers/movesNumber.js
@@ -2,6 +2,8 @@ export const movesNumber = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT_MOVES_COUNTER':
       return action.movesNumber;
+    case 'RESET_GAME':
+      return 0;
     default:
       return state;
   }
