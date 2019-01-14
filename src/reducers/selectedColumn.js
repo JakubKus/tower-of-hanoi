a014ef7be2367bbe5b6f047260fec29b4a740607
Kuba <kuba.mistrz@o2.pl>
2019-01-15 00:17:14 +0100

    added case which unselects selected column

--- src/reducers/selectedColumn.js
+++ src/reducers/selectedColumn.js
@@ -2,6 +2,8 @@ export const selectedColumn = (state = -1, action) => {
   switch (action.type) {
     case 'SELECT_COLUMN':
       return action.id;
+    case 'RESET_GAME':
+      return -1;
     default:
       return state;
   }
