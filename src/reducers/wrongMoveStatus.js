ba949208c422249ca6eb8b2aa8e6f7c26b5d35d4
Kuba <kuba.mistrz@o2.pl>
2019-01-15 00:16:44 +0100

    added case which clears wrongMove

--- src/reducers/wrongMoveStatus.js
+++ src/reducers/wrongMoveStatus.js
@@ -2,6 +2,8 @@ export const wrongMoveStatus = (state = false, action) => {
   switch (action.type) {
     case 'DID_WRONG_MOVE':
       return action.status;
+    case 'RESET_GAME':
+      return false;
     default:
       return state;
   }
